688add352949e468b97fc0ed7ab656cc063c039d
fitowashere <fito_washere@icloud.com>
2023-09-20 10:48:35 -0400

    fixing output

--- main.c
+++ main.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
 {
 	if (argc != 2)
 	{
-		fprintf(stderr, "USAGE: %s monty file\n", argv[0]);
+		fprintf(stderr, "USAGE: %s monty file\n", argv[1]);
 		return (EXIT_FAILURE);
 	}
 	FILE *file = fopen(argv[1], "r");
